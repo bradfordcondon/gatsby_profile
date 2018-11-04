c5f1501d77f2bab45ae828b759cc173118f4b249
bradfordcondon <bradford.condon@gmail.com>
2018-11-03 21:08:15 -0400

    site load

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,70 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
-import "../styles/layout-override.css";
+import React from "react"
+import { css } from "react-emotion"
+import { Link, graphql } from "gatsby"
+//import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
 
+export default ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <h1
+          className={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}
+        >
+          Amazing Pandas Eating Things
+        </h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link
+              to={node.fields.slug}
+              className={css`
+                text-decoration: none;
+                color: inherit;
+              `
+}
+            >
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+              <h3
+              >
+                {node.frontmatter.title}{" "}
+                <span
+                  className={css`
+                    color: #bbb;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-
-export default IndexPage
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
