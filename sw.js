e0179408465de9b9b1af400b3cd0ac4ae84a7610
bradfordcondon <bradford.condon@gmail.com>
2018-12-11 21:27:59 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fd654d065e68f3df2b9e.js"
+    "url": "webpack-runtime-27d9aa67d3b93b4234bd.js"
   },
   {
     "url": "app.319712d3e7fffa8163a7.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "950d970b48a5adf81f84e78338f6af4f"
+    "revision": "526e6778b747bcecaf3878baad32b6fa"
   },
   {
     "url": "1.bf48bd1753571215e4b7.css"
