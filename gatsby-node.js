e7cff4cec266cfe5fe88ea0e4edbf95fc5109549
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-12-15 09:45:59 -0500

    Some styling changes with some other odd stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,28 @@
 const path = require(`path`)
 const _ = require("lodash")
 
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const {createFilePath} = require(`gatsby-source-filesystem`)
+const createPaginatedPages = require("gatsby-paginate");
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
+exports.onCreateNode = ({node, getNode, actions}) => {
+    const {createNodeField} = actions
+    if (node.internal.type === `MarkdownRemark`) {
+        const slug = createFilePath({node, getNode, basePath: `pages`})
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+        })
+    }
 }
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  const tagTemplate = path.resolve("src/templates/tags.js")
-  const blogPostTemplate = path.resolve("src/templates/blog-post.js")
+exports.createPages = ({graphql, actions}) => {
+    const {createPage} = actions
+    const tagTemplate = path.resolve("src/templates/tags.js")
+    const blogPostTemplate = path.resolve("src/templates/blog-post.js")
 
-  return new Promise((resolve, reject) => {
-    graphql(`
+    return new Promise((resolve, reject) => {
+        graphql(`
       {
           allMarkdownRemark {
             edges {
@@ -37,45 +38,54 @@ exports.createPages = ({ graphql, actions }) => {
           }
         }
     `
-).then(result => {
-  if (result.errors) {
-    return Promise.reject(result.errors)
-  }
-  const posts = result.data.allMarkdownRemark.edges
+        ).then(result => {
+            if (result.errors) {
+                return Promise.reject(result.errors)
+            }
+            const posts = result.data.allMarkdownRemark.edges
 
-  posts.forEach(({ node }) => {
-     createPage({
-       path: node.fields.slug,
-       component: blogPostTemplate,
-       context: {
-         // Data passed to context is available
-         // in page queries as GraphQL variables.
-         slug: node.fields.slug,
-       },
-     })
-   })
+            createPaginatedPages({
+                edges: posts,
+                createPage: createPage,
+                pageTemplate: "src/pages/index.js",
+                pageLength: 10, // This is optional and defaults to 10 if not used
+                pathPrefix: "", // This is optional and defaults to an empty string if not used
+                context: {} // This is optional and defaults to an empty object if not used
+            });
 
-   // Tag pages:
-   let tags = []
-   // Iterate through each post, putting all found tags into `tags`
-   _.each(posts, edge => {
-     if (_.get(edge, "node.frontmatter.tags")) {
-       tags = tags.concat(edge.node.frontmatter.tags)
-     }
-   })
-   // Eliminate duplicate tags
-   tags = _.uniq(tags)
-   // Make tag pages
-    tags.forEach(tag => {
-      createPage({
-        path: `/tags/${_.kebabCase(tag)}/`,
-        component: tagTemplate,
-        context: {
-          tag,
-        },
-      })
+            posts.forEach(({node}) => {
+                createPage({
+                    path: node.fields.slug,
+                    component: blogPostTemplate,
+                    context: {
+                        // Data passed to context is available
+                        // in page queries as GraphQL variables.
+                        slug: node.fields.slug,
+                    },
+                })
+            })
+
+            // Tag pages:
+            let tags = []
+            // Iterate through each post, putting all found tags into `tags`
+            _.each(posts, edge => {
+                if (_.get(edge, "node.frontmatter.tags")) {
+                    tags = tags.concat(edge.node.frontmatter.tags)
+                }
+            })
+            // Eliminate duplicate tags
+            tags = _.uniq(tags)
+            // Make tag pages
+            tags.forEach(tag => {
+                createPage({
+                    path: `/tags/${_.kebabCase(tag)}/`,
+                    component: tagTemplate,
+                    context: {
+                        tag,
+                    },
+                })
+            })
+            resolve()
+        })
     })
-      resolve()
-    })
-  })
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,41 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import {
-  Collapse,
-  Jumbotron,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Button
-} from "reactstrap";
+import {Link} from 'gatsby'
+import {Jumbotron, InputGroup, InputGroupAddon, Form, Input, Button} from "reactstrap";
+import algoliasearch from 'algoliasearch/lite';
+import {InstantSearch, SearchBox, Hits} from 'react-instantsearch-dom';
 
 import Navcomponent from './Navcomponent'
 
+const searchClient = algoliasearch(
+    'Y8SHL6PF2G',
+    '5cb958188465bc857e6052fa967bdab0'
+);
+
 const Header = () => (
-  <div>
-  <Jumbotron color = 'red' style={{paddingBottom: '0px'}}>
-    <Link to="/"
-      style={{
-        color: 'black',
-        textDecoration: 'none',
-      }}
-    >
-       <h1 className="display-3"> Bradford Condon, PhD</h1>
-       <p className="lead">Bionformatics, Data Science, Web & Mobile Development</p>
-        </Link>
-    <Navcomponent/>
-    </Jumbotron>
+    <div>
+        <Jumbotron className={'pb-0 pt-3 border-bottom'} style={{borderColor: '#ccc'}}>
+            <div className="container">
+                <div className="d-flex justify-content-between">
+                    <Link to="/"
+                          style={{
+                              color: 'black',
+                              textDecoration: 'none',
+                          }}
+                    >
+                        <h1 className="display-6 font-weight-bold"> Bradford Condon, PhD</h1>
+                        <p className="lead">Bionformatics, Data Science, Web & Mobile Development</p>
+                    </Link>
+                    <InstantSearch
+                        indexName="prod_gatsby_bcondon"
+                        searchClient={searchClient}
+                    >
+                        <SearchBox />
+                        <Hits />
+                    </InstantSearch>
+                </div>
+                <Navcomponent/>
+            </div>
+        </Jumbotron>
     </div>
 );
 
