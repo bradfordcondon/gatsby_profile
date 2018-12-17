15068eecc6833b7b7809fa8aa810ad5eab68b031
bradfordcondon <bradford.condon@gmail.com>
2018-12-16 23:10:28 -0500

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,13 @@ module.exports = {
       },
     },
     {
+   resolve: `gatsby-plugin-google-analytics`,
+   options: {
+   // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+     trackingId: "UA-71775012-1",
+   },
+ },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/posts`,
